788c6ac5052303e91bd7ebbc0dba0c113cd67ae9
Mali <malihealam2023@gmail.cim>
2025-07-17 13:49:02 +0200

    Complete task number 10

---
+++ src/cydeo/com/Mali/Task10_sortArray.java
@@ -0,0 +1,34 @@
+package cydeo.com.Mali;
+
+public class Task10_sortArray {
+    public static int[] sort(int[] arr) {
+
+        for (int i = 0; i < arr.length-1; i++) {
+
+            for (int j = 0; j < arr.length-1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                }
+            }
+
+        }
+
+        return arr;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {10, 9, 8, 7};
+        arr = sort(arr);
+
+
+        for (int eachnum : arr) {
+            System.out.print(eachnum + " ");
+        }
+    }
+}
+
