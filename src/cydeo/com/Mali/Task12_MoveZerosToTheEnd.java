40e2bdd14488dc9bcc171334e5643d7426d525aa
Mali <malihealam2023@gmail.cim>
2025-07-31 09:56:41 +0200

    Complete task number 12

---
+++ src/cydeo/com/Mali/Task12_MoveZerosToTheEnd.java
@@ -0,0 +1,27 @@
+package cydeo.com.Mali;
+
+import java.util.Arrays;
+
+public class Task12_MoveZerosToTheEnd {
+    /*
+   Array -- Move Zeros to the End
+Write a method that can move all the zeros to  last indexes of the array (Do Not Use Sort Method)
+Ex:
+input:  {1,0,2,0,3,0,4,0};
+output: [1, 2, 3, 4, 0, 0, 0, 0]
+ */
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(moveZeros(1,0,2,0,3,0,4,0)));
+
+    }
+    public static int[] moveZeros(int...nums){
+        int[] moved = new int[nums.length];
+        int index = 0;
+        for (int each : nums) {
+          if(each !=0){
+              moved[index++]=each;
+          }
+        }
+        return moved;
+    }
+}
