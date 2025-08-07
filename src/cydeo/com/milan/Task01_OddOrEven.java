c5e5b7b464792b877462ba3c8d87b14d08e47dc3
milan-s123 <marsijal87@yahoo.com>
2025-08-07 18:16:25 +0200

    Task 01 is done

--- src/cydeo/com/milan/Task01_OddOrEven.java
+++ src/cydeo/com/milan/Task01_OddOrEven.java
@@ -1,4 +1,24 @@
 package cydeo.com.milan;
 
+import java.util.Scanner;
+
 public class Task01_OddOrEven {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your number:");
+        int num = scanner.nextInt();
+
+        if (num <= 0) {
+            System.err.println(num + " number is invalid");
+        } else if (num % 2 == 0) {
+            System.out.println(num + " number is even");
+        } else {
+            System.out.println(num + " number is odd");
+        }
+
+    }
+
 }
+
